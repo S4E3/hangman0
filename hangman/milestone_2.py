e91ac22c967efa5427ca9085d08c77eb2399e2e6
Saeed <kingsaeed99@gmail.com>
2023-12-03 22:43:55 +0000

    complete milestone 2 task 4

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -6,4 +6,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("please enter a single character: ")
-print(guess)
+print("your character is ", guess)
+if(len(guess) == 1):
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
