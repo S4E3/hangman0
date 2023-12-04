9f9be5710cddfdb19a18d5ef8f56d8c54a78d569
Saeed <kingsaeed99@gmail.com>
2023-12-04 00:47:41 +0000

    complete milestone 2 task 7

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,13 +1,13 @@
 import random
-word_list = ["blackberry", "strawberry", "apple", "banana", "orange"]
-print(word_list)
+favourite_fruit_list = ["blackberry", "strawberry", "apple", "banana", "orange"]
+print(favourite_fruit_list)
  
-word = random.choice(word_list)
-print(word)
+random_fruit = random.choice(favourite_fruit_list)
+print(random_fruit)
 
-guess = input("please enter a single character: ")
-print("your character is ", guess)
-if(len(guess) == 1):
-    print("Good guess!")
+user_character_input = input("please enter a single character: ")
+print("your character is ", user_character_input)
+if(len(user_character_input) == 1):
+    print("Good user_character_input!")
 else:
     print("Oops! That is not a valid input.")
