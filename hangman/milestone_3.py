e7817f89386de94683158efd50a569a995251225
Saeed <kingsaeed99@gmail.com>
2023-12-05 20:53:02 +0000

    complete milestone 3 task 3

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -7,20 +7,29 @@ print(favourite_fruit_list)
 random_fruit = random.choice(favourite_fruit_list)
 print(random_fruit)
 
-fruit = "apple"
 
-while(condition == True):
-    guess = input("please guess a letter: ")
-    if(len(guess) == 1):
-        if(guess.isalpha() == True):
-            break
+def check_guess(guess):
+    guess_lower = str(guess).lower()
+    if(guess_lower in random_fruit):
+        print("Good guess! ", guess, " is in the word.")
+    else:
+        print("Sorry, ", guess, " is not in the word. Try again.")
+    
+def ask_for_input():
+    while(condition == True):
+        guess = input("please guess a letter: ")
+        if(len(guess) == 1):
+            if(guess.isalpha() == True):
+                break
+            else:
+                print("Invalid letter. Please, enter a single alphabetical character.")        
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_input()
+ 
+
+
     
-print("while loop exited")
-if(guess in fruit):
-    print("Good guess! ", guess, " is in the word.")
-else:
-    print("Sorry, ", guess, " is not in the word. Try again.")
+    
